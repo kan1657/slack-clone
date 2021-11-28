5f9bab8d94690a2f86dc50dc153db96f3d011ae6
Kan <kan1657@hotmail.com>
2021-11-28 13:29:49 +0700

    Fix firestore version problem

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAnkdZmKVPR07h0FaWk5mIsfcoj6XYHuUU",
+  authDomain: "slack-clone-1ae69.firebaseapp.com",
+  projectId: "slack-clone-1ae69",
+  storageBucket: "slack-clone-1ae69.appspot.com",
+  messagingSenderId: "412229021288",
+  appId: "1:412229021288:web:a0d198dcfd1a4c8dec95dd",
+};
